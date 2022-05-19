6424574005403de809c0d42f9bd68ca32226f27e
laxmisari <laxmisari69@gmail.com>
2022-05-18 19:27:04 -0700

    sauce project

---
+++ src/test/java/com/JavaBasics/JavaBasics.java
@@ -0,0 +1,36 @@
+package com.JavaBasics;
+
+import java.lang.reflect.Array;
+
+public class JavaBasics {
+
+	public static void main(String[] args) {
+		
+		int Mynum = 200;
+		String Myname = "laxmi";
+		boolean sum = true;
+		double num = 125.65;
+		 System.out.println("My number is :" +Mynum);
+		 System.out.println("My name is :" +Myname );
+		 System.out.println("My sum is :" +sum);
+		 System.out.println("My number is :" +num);
+		 
+		 
+	
+		 
+	}
+	public  static void arraydisp()
+	{
+		int[] arra1 = new int[3];
+		arra1[0] = 1;
+		arra1[1] = 2;
+		arra1[2] = 3;
+		
+		System.out.println("Array numbers are:" + arra1[3] );
+		
+		
+	}
+	
+	
+
+}
